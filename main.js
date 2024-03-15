320da38d23cf4f003b7a807f3635fa0424a802ed
Thomas Junghans <thomas.junghans@gmail.com>
2024-03-15 23:31:01 +0100

    chore: add eslint

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
 class MarketPriceTicker extends HTMLElement {
-    static observedAttributes = ["ccy-pair"];
+  static observedAttributes = ['ccy-pair']
 
-    constructor() {
-        super();
-    }
+  connectedCallback () {
+    this.ccyPair = this.getAttribute('ccy-pair')
+    this.ticker = window.setInterval(() => {
+      this.#setText()
+    }, 1000)
+    this.#setText()
+  }
 
-    connectedCallback() {
-        this.ccyPair = this.getAttribute("ccy-pair");
-        this.ticker = window.setInterval(() => {
-            this.#setText();
-        }, 1000)
-        this.#setText();
-    }
+  disconnectedCallback () {
+    window.clearInterval(this.ticker)
+  }
 
-    disconnectedCallback() {
-        window.clearInterval(this.ticker);
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (name === 'ccy-pair') {
+      this.ccyPair = newValue
+      this.#setText()
     }
+  }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name === "ccy-pair") {
-            this.ccyPair = newValue;
-            this.#setText();
-        }
-    }
-
-    #setText() {
-        this.innerHTML = `market price ${this.ccyPair}:  ${Math.random()}`;
-    }
+  #setText () {
+    this.innerHTML = `market price ${this.ccyPair}:  ${Math.random()}`
+  }
 }
 
-customElements.define("mkt-price-ticker", MarketPriceTicker);
-
+customElements.define('mkt-price-ticker', MarketPriceTicker)
